c9c245a92c5927de582567cc9ff333c145634e0f
Berkan Unal <Berkanunal@gmail.com>
2022-04-29 21:29:41 +0300

    Step2: Integrate the bull-board/ui

--- start/routes.ts
+++ start/routes.ts
@@ -21,3 +21,24 @@
 import Route from '@ioc:Adonis/Core/Route'
 
 Route.get('/', 'DummiesController.get')
+
+import { appRoutes } from '@bull-board/api/dist/src/routes'
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+Route.group(() => {
+  generateBullBoardEntryRoutes()
+}).prefix('/bull-board')
+
+function generateBullBoardEntryRoutes() {
+  const { route: entryRoute, method } = appRoutes.entryPoint
+
+  const routes = Array.isArray(entryRoute) ? entryRoute : [entryRoute]
+
+  const handler = async ({ view }: HttpContextContract) => {
+    return view.render('bull_board')
+  }
+
+  return routes.map((route) => {
+    Route.route(route, [method.toUpperCase()], handler)
+  })
+}
